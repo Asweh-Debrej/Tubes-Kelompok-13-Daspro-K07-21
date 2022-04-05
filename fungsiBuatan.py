472be27d4eb08ea481613f41cedddfd03df6e108
Asweh Debrej <aswehdebrej@gmail.com>
2022-04-05 14:52:47 +0700

    Lupa upload

--- fungsiBuatan.py
+++ fungsiBuatan.py
@@ -1,6 +1,11 @@
 import fungsiDasar as fd
 
-def cipher(string, seed = 16, modifier = 6969):
+_seed = 16
+_cipherModifier = 6
+_maxOrd = 1114112
+
+
+def cipher(string, seed= _seed, modifier= _cipherModifier):
     """Menyandi string masukan. Hasil akan bergantung pada seed yang diberikan.
     
     Seed harus berupa faktor dari 1114112.
@@ -9,22 +14,42 @@ def cipher(string, seed = 16, modifier = 6969):
     """
     if 1114112 % seed != 0:
         print("""
-        Peringatan : Seed yang dimasukkan buakn salah satu faktor 1114112.
+        Peringatan : Seed yang dimasukkan bukan salah satu faktor 1114112.
         Seed akan diubah menjadi 16.
         """)
-        seed = 16
+        seed = _seed
 
     strArr = list(string)
     for i in range(fd.len(strArr)):
         ordinate = ord(strArr[i])
-        ciphOrd = (ordinate//seed + 1) * seed + ordinate % seed + modifier
-        strArr[i] = chr(ciphOrd % 1114112)
+        ciphOrd = (ordinate//seed + 1) * seed - ordinate % seed - 1 + modifier
+        strArr[i] = chr(ciphOrd % _maxOrd)
 
     string = fd.join(strArr, '')
     return string
 
+def decipher(string, seed= _seed, modifier= _cipherModifier):
+    """Menguraikan string masukan. Hasil akan bergantung pada seed yang diberikan.
+    
+    Seed harus berupa faktor dari 1114112.
 
+    Modifier berupa angka bebas
+    """
+    if 1114112 % seed != 0:
+        print("""
+        Peringatan : Seed yang dimasukkan bukan salah satu faktor 1114112.
+        Seed akan diubah menjadi 16.
+        """)
+        seed = _seed
 
+    strArr = list(string)
+    for i in range(fd.len(strArr)):
+        ordinate = ord(strArr[i]) - modifier
+        ciphOrd = (ordinate//seed + 1) * seed - ordinate % seed - 1
+        strArr[i] = chr(ciphOrd % _maxOrd)
+
+    string = fd.join(strArr, '')
+    return string
 
 if __name__ == "__main__":                  # Kalau mau tes kode silahkan ubah isi ini dan run codenya
-    print(cipher("abcdefghijklmn", modifier= 1))
+    print(decipher(cipher("abcdefg")))
