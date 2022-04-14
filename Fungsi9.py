673d52a38fbf2187a797641603947a9b35d774e5
KevinIgnatiusW <102893759+KevinIgnatiusW@users.noreply.github.com>
2022-04-14 19:37:45 +0700

    Add files via upload

--- Fungsi9.py
+++ Fungsi9.py
@@ -4,21 +4,23 @@ def list_game():
     username = username[] //username dari fungsi 3
      x =len(_possession) // len dr fungsiDasar
      k= len(_gameData)
-     t=0 //variable nonor 
+     t=0 //variable nomor 
      if(x==0):
+         print()
          print("Maaf, kamu belum membeli game. Ketik perintah beli_game untuk beli.")
          return 
      for m in range (x):
              if(_possession[m][1]==username):
                  for j in range (k):
                      if(_possession[m][0]==_gamedata[j][0]):
-                        t+=1
+                         print()
+                         t+=1
                          print(t,end=". ")
                          print(_gamedata[j][0],end=" | ")
                          print(_gamedata[j][1],end=" | ")
                          print(_gamedata[j][2],end=" | ")
                          print(_gamedata[j][3],end=" | ")
                          print(_gamedata[j][4],end="")
-                 print()
+                 
 
                          
