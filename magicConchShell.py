65d89995d39d0f976837ca9e59950ca7c5888432
jonnjonnjo <88668123+jonnjonnjo@users.noreply.github.com>
2022-04-14 15:52:00 +0700

    Selesai

---
+++ magicConchShell.py
@@ -0,0 +1,15 @@
+import time
+
+a = 2
+m = 6
+rn = time.time()
+c = 3
+
+arr = ["Ya","Tidak","Bisa Jadi","Mungkin","Tentunya","Tidak Mungkin"]
+
+
+k = input("Apa pertanyaanmu ? ")
+
+jawab = round(((a)*(rn) + (c))) % m
+
+print(arr[jawab])
