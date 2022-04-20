bb9b6ec881cda1dae1e8ca24789589026fdf5132
jonnjonnjo <88668123+jonnjonnjo@users.noreply.github.com>
2022-04-21 06:58:26 +0700

    selesai

--- listingGame.py
+++ listingGame.py
@@ -1,11 +1,11 @@
 import fungsiDasar as fd
-
+import fungsiBuatan as fb
 
 def selection_sort(_skemaSorting,_arrData):
     # Melakukan selection sort dan mengprint hasilnya
     _banyakData = fd.len(_arrData)
 
-    for x in range(_banyakData-1):
+    for x in range(1,banyakData):
         _indeksMaks = x
 
         for y in range(x+1,_banyakData):
@@ -44,31 +44,29 @@ def selection_sort(_skemaSorting,_arrData):
         _arrData[x][3]  = _temp3
         _arrData[x][4]  = _temp4
 
-    # cari string terpanjang
-    _terpanjang = 0
-    for x in range(_banyakData):
-        for y in range(6):
-            if(len(str(_arrData[x][y])) > _terpanjang):
-                _terpanjang = len(str(_arrData[x][y]))
-
-    s = '{: ^' + str(_terpanjang) +'}'
-
-    for x in range(_banyakData):
-        print(str(x + 1) + '. ' + s.format(_arrData[x][0]) + " | " + s.format(_arrData[x][1]) + " | " + s.format(_arrData[x][4]) + "\t | " + s.format(_arrData[x][2]) + "\t | " + s.format(_arrData[x][3]) + "\t | " + s.format(_arrData[x][5]))
+      #print matriks
+    _lengths = fb.maxLengths(_arrData)
+    line1 = "| {0[0]:^{1[0]}} | {0[1]:^{1[1]}} | {0[4]:^{1[4]}} | {0[2]:^{1[2]}} | {0[3]:^{1[3]}} | {0[5]:^{1[5]}} |".format(_arrData[0],_lengths)
+    print('_'*fd.len(line1))
+    print(line1)
+    print("| {} | {} | {} | {} | {} | {}".format(_lengths[0]*'-',_lengths[1]*'-',_lengths[4]*'-',_lengths[2]*'-',_lengths[3]*'-',_lengths[5]*'-'))
+    for i in range(1, fd.len(_arrData)):
+        print("| {0[0]:<{1[0]}} | {0[1]:<{1[1]}} | {0[4]:<{1[4]}} | {0[2]:<{1[2]}} | {0[3]:<{1[3]}} | {0[5]:<{1[5]}} |".format(_arrData[i], _lengths))
+    print('‾'*fd.len(line1))
 
 
 def listing_game(game):
     # Fungsi utama
     _banyakData = fd.len(game)
-    _arrData= [[0 for x in range(6)] for x in range(1,_banyakData)]
+    _arrData= [[0 for x in range(6)] for x in range(_banyakData)]
 
-    for x in range(1,_banyakData):
-        _arrData[x-1][0]  = game[x][0]
-        _arrData[x-1][1]  = game[x][1]
-        _arrData[x-1][2]  = game[x][2]
-        _arrData[x-1][3]  = game[x][3]
-        _arrData[x-1][4]  = game[x][4]
-        _arrData[x-1][5]  = game[x][5]
+    for x in range(_banyakData):
+        _arrData[x][0]  = game[x][0]
+        _arrData[x][1]  = game[x][1]
+        _arrData[x][2]  = game[x][2]
+        _arrData[x][3]  = game[x][3]
+        _arrData[x][4]  = game[x][4]
+        _arrData[x][5]  = game[x][5]
 
     _skemaSorting = input("Skema sorting : ")
 
