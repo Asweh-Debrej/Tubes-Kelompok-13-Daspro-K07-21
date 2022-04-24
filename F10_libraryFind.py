e32535e05e7ec1f6ff4bdc8ea9d515e8e6271b57
raniakiranaa <102893723+raniakiranaa@users.noreply.github.com>
2022-04-24 14:35:03 +0700

    Update F10_libraryFind.py

--- F10_libraryFind.py
+++ F10_libraryFind.py
@@ -1,92 +1,91 @@
 import C01_fungsiDasar as fd
 
 def search_my_game(_possession,_gamedata,_loggedUser):
-     
-     _username = _loggedUser[0]
-     _panjangPossession =fd.len(_possession) 
-     _panjangGame = fd.len(_gamedata)
-     _idGame=input("Masukkan ID Game: ")
-     _tahunRilis=input("Masukkan Tahun Rilis Game: ")
+
+     username = _loggedUser[0]
+     x =fd.len(_possession) 
+     k= fd.len(_gamedata)
+     str1=input("Masukkan ID Game: ")
+     str2=input("Masukkan Tahun Rilis Game: ")
      print("Daftar game pada inventory yang memenuhi kriteria: ")
-     
-     _count=0
-     if(_idGame=='' and _tahunRilis ==''):
-          
-          for m in range (_panjangPossession):
-               
-               if(_possession[m][1]==_username):
-                    
-                    for j in range (_panjangGame):
+
+     t=0
+     if(str1=='' and str2 ==''):
+
+          for m in range (x):
+
+               if(_possession[m][1]==username):
+
+                    for j in range (k):
                          if(_possession[m][0]==_gamedata[j][0]):
                               print()
-                              _count+=1
-                              print(_count,end=". ")
+                              t+=1
+                              print(t,end=". ")
                               print(_gamedata[j][0],end=" | ")
                               print(_gamedata[j][1],end=" | ")
                               print(_gamedata[j][2],end=" | ")
                               print(_gamedata[j][3],end=" | ")
                               print(_gamedata[j][4],end="")
-                 
-          if(_count==0):
-               print()
-               print("Tidak ada game pada inventory-mu yang memenuhi kriteria.",end="")
-     elif(_idGame!='' and _tahunRilis!=''):
-
-          for m in range (_panjangPossession):
-               if(_possession[m][1]==_username):
-                 
-                    for j in range (_panjangGame):
-                         if(_possession[m][0]==_gamedata[j][0] and _idGame.lower() == _gamedata[j][0].lower() and int(_tahunRilis) ==_gamedata[j][3] ):
-                              print()
-                              _count+=1
-                              print(_count,end=". ")
-                              print(_gamedata[j][0],end=" | ")
-                              print(_gamedata[j][1],end=" | ")
-                              print(_gamedata[j][2],end=" | ")
-                              print(_gamedata[j][3],end=" | ")
-                              print(_gamedata[j][4],end="")
-
-          if(_count==0):
-               print()
-               print("Tidak ada game pada inventory-mu yang memenuhi kriteria.",end="")
-
-     elif(_idGame!='' and _tahunRilis==''):
-
-          for m in range (_panjangPossession):
-               if(_possession[m][1]==_username):
-                 
-                    for j in range (_panjangGame):
-                         if(_possession[m][0]==_gamedata[j][0] and _idGame.lower() == _gamedata[j][0].lower()):
-                              print()
-                              _count+=1
-                              print(_count,end=". ")
-                              print(_gamedata[j][0],end=" | ")
-                              print(_gamedata[j][1],end=" | ")
-                              print(_gamedata[j][2],end=" | ")
-                              print(_gamedata[j][3],end=" | ")
-                              print(_gamedata[j][4],end="")
-
-          if(_count==0):
-               print()
-               print("Tidak ada game pada inventory-mu yang memenuhi kriteria.",end="")
-
-     elif(_idGame=='' and _tahunRilis!=''):
-
-          for m in range (_panjangPossession):
-               if(_possession[m][1]==_username):
-                 
-                    for j in range (_panjangGame):
-                         if(_possession[m][0]==_gamedata[j][0] and  int(_tahunRilis) ==_gamedata[j][3]):
-                              print()
-                              _count+=1
-                              print(_count,end=". ")
-                              print(_gamedata[j][0],end=" | ")
-                              print(_gamedata[j][1],end=" | ")
-                              print(_gamedata[j][2],end=" | ")
-                              print(_gamedata[j][3],end=" | ")
-                              print(_gamedata[j][4],end="")
-
-          if(_count==0):
-               print()
-               print("Tidak ada game pada inventory-mu yang memenuhi kriteria.",end="")
                     
+          if(t==0):
+               print()
+               print("Tidak ada game pada inventory-mu yang memenuhi kriteria.",end="")
+     elif(str1!='' and str2!=''):
+
+          for m in range (x):
+               if(_possession[m][1]==username):
+
+                 for j in range (k):
+                    if(_possession[m][0]==_gamedata[j][0] and str1.lower() == _gamedata[j][0].lower() and int(str2) ==_gamedata[j][3] ):
+                         print()
+                         t+=1
+                         print(t,end=". ")
+                         print(_gamedata[j][0],end=" | ")
+                         print(_gamedata[j][1],end=" | ")
+                         print(_gamedata[j][2],end=" | ")
+                         print(_gamedata[j][3],end=" | ")
+                         print(_gamedata[j][4],end="")
+
+          if(t==0):
+               print()
+               print("Tidak ada game pada inventory-mu yang memenuhi kriteria.",end="")
+
+     elif(str1!='' and str2==''):
+
+          for m in range (x):
+               if(_possession[m][1]==username):
+
+                 for j in range (k):
+                    if(_possession[m][0]==_gamedata[j][0] and str1.lower() == _gamedata[j][0].lower()):
+                         print()
+                         t+=1
+                         print(t,end=". ")
+                         print(_gamedata[j][0],end=" | ")
+                         print(_gamedata[j][1],end=" | ")
+                         print(_gamedata[j][2],end=" | ")
+                         print(_gamedata[j][3],end=" | ")
+                         print(_gamedata[j][4],end="")
+
+          if(t==0):
+               print()
+               print("Tidak ada game pada inventory-mu yang memenuhi kriteria.",end="")
+
+     elif(str1=='' and str2!=''):
+
+          for m in range (x):
+               if(_possession[m][1]==username):
+
+                 for j in range (k):
+                    if(_possession[m][0]==_gamedata[j][0] and  int(str2) ==_gamedata[j][3]):
+                         print()
+                         t+=1
+                         print(t,end=". ")
+                         print(_gamedata[j][0],end=" | ")
+                         print(_gamedata[j][1],end=" | ")
+                         print(_gamedata[j][2],end=" | ")
+                         print(_gamedata[j][3],end=" | ")
+                         print(_gamedata[j][4],end="")
+
+          if(t==0):
+               print()
+               print("Tidak ada game pada inventory-mu yang memenuhi kriteria.",end="")
